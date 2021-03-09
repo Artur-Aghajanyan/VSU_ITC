241af040da10992e6729e6b59adcb4f86f99f370
Artur Badalyan <badalyannaaa@gmail.com>
2021-03-09 23:17:40 +0400

    Create branch,finished homework(Fibonacci,Factorial,Arhimetic mean)

---
+++ Artur_Badalyan/09Mar2021/arithmeticMean/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "mean.h"
+using namespace std;
+
+
+int a;
+
+int main(){
+
+	
+	cout << "Enter array ";
+	cin >> a;
+
+	mean(a);
+
+}
---
+++ Artur_Badalyan/09Mar2021/arithmeticMean/mean.h
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+
+
+int mean(int num);
