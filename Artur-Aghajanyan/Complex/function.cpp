b44ec8d3c33db052422a8dd21e98babb31a94aeb
Artur Aghajanyan <artur.aghajanyan1997@mail.ru>
2021-03-19 23:40:31 +0400

    any update

--- Artur-Aghajanyan/Complex/function.cpp
+++ Artur-Aghajanyan/Complex/function.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 /*
  *	Static Function
  *	name foo
@@ -9,18 +7,4 @@ int foo() {
 	static int i = 0;
 	++i;
 	return i;
-}
-
-iint main() {
-	/*
-	 *	I call foo function 5 time
-	 *	for the sixth time I print resalt
-	 * */
-	for(int i = 0; i < 5; ++i)
-		foo();
-	
-	// sixth time for calling foo function
-	std::cout << foo() << std::endl;\
-
-	return 0;
 }
--- Artur-Aghajanyan/Complex/main.cpp
+++ Artur-Aghajanyan/Complex/main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 #include "complex.h"
+#include "function.cpp"
 
 int main() {
+	/*
+	 *	I call foo function 5 time
+	 *	for the sixth time I print resalt
+	 * */
+	for(int i = 0; i < 5; ++i)
+		foo();
+	
+	// sixth time for calling foo function
+	std::cout << "foo function result after calling 6th time = " <<foo() << "\n\n";
+	/*
+	 * End call foo function
+	 * */
+
+	/*
+	 *Starting make Complex objects
+	 * */
+		
 	// Creating 2 objects from class Complex
 	Complex<int> c1(10,25), c2(5, 6);
 
@@ -31,5 +49,8 @@ int main() {
 	std::cout << "\nObject two\n";
 	std::cin >> c2;
 	std::cout << "\n(a1 + b1i)*(a2 + b2i) =  " << c1 * c2 << std::endl << std::endl;
+	/*
+	 *End make complex object
+	 * */
 	return 0;
 }
