7c01efafea91ec90641d4f8e308edfa98bbd90bd
ZinaJS <79909126+ZinaJS@users.noreply.github.com>
2021-03-20 16:37:21 +0400

    Create complex.h
    Created a header file

---
+++ Zina1/complex.h
@@ -0,0 +1,33 @@
+#include <ostream>
+#include <istream>
+
+class Complex {
+	private:
+		int real;
+		int image;
+	public:
+		/*
+		    Constructors
+		*/
+		Complex();
+		Complex(int real, int image);
+		Complex(const Complex& number2);
+		
+		/*
+		    <<, >>, +, -, * operators overloaded
+		*/
+		friend std :: ostream& operator << (std :: ostream& output, const Complex& number);
+		friend std :: istream& operator >> (std :: istream& input, Complex& number);
+		Complex operator + (const Complex& number);
+		Complex operator - (const Complex& number);
+		Complex operator * (const Complex& number);
+		
+		/*
+		    Functions
+		*/
+		int print();
+		void setReal(int r);
+		void setImage(int i);
+		int getReal();
+		int getImage();
+};
