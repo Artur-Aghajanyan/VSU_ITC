8cf17a20f1d5b884fe78d166bc4dad22ac474350
ZinaJS <79909126+ZinaJS@users.noreply.github.com>
2021-03-21 15:50:24 +0400

    Update clexmain.cpp

--- Zina1/clexmain.cpp
+++ Zina1/clexmain.cpp
@@ -18,29 +18,30 @@ int main() {
 	/*
 		Printed number1 and number2.
 	*/
-	std :: cout << "Output Re and Im of first complex number: " << number1 << std :: endl;
-	std :: cout << "Output Re and Im of second complex number: " << number2 << std :: endl;
+	std :: cout << "Re and Im of first complex number: " << number1 << std :: endl;
+	std :: cout << "Re and Im of second complex number: " << number2 << std :: endl;
 	
 	/*
 		Printed the sum, the difference and the product of number1 and number2.
 	*/
-	std :: cout << "Output the sum of two complex numbers: " << number1 + number2 << std :: endl;
-	std :: cout << "Output the difference of two complex numbers: " << number1 - number2 << std :: endl;
-	std :: cout << "Output the product of two complex numbers " << number1 * number2 << std :: endl;
+	std :: cout << "The sum of two complex numbers: " << number1 + number2 << std :: endl;
+	std :: cout << "The difference of two complex numbers: " << number1 - number2 << std :: endl;
+	std :: cout << "The product of two complex numbers " << number1 * number2 << std :: endl;
 	
 	/*
 		Made a variable (number3) with a copy constructor and coppied number0 in it. 
 		Then printed the value of number3.
 	*/
 	Complex number3 = number0;
-	std :: cout << "Output the result of using copy constructor: " << number3 << std :: endl;
+	std :: cout << "The result of using copy constructor: " << number3 << std :: endl;
 	
 	/*
         Printing the count of calls of print() function.
     */
-	Complex a;
-	std :: cout << a.print();
-	std :: cout << a.print();
-	std :: cout << a.print();
-	std :: cout << a.print();
+	print();
+	print();
+	print();
+	print();
+	print();
+	std :: cout << "The count of calls of print() function is " << print();
 }
