e022cc2a0d24e0e163b10da2a7a2fe515191a2e6
Van Marukyan <van.marukyan.1997@gmail.com>
2021-04-02 12:00:43 +0200

    Asembler Homvork

---
+++ Van/asembl_homw/asmHomv/digits_add_input.asm
@@ -0,0 +1,33 @@
+%include "print_integers.asm"
+
+section .data
+    text1 db "first summable - "
+    len1 equ $- text1
+    text2 db "second summable - "
+    len2 equ $- text2      
+    text3 db   "num1 + num2 = " 
+    len3 equ $- text3
+              
+    text4 db 10
+   
+                                
+section .bss
+  
+    num1 resb 2
+    num2 resb 2
+    res resb 1
+    
+section .text
+global _start
+_start:
+    print text1, len1
+    get num1, 2
+    print text2, len2
+    get num2, 2
+    print text3, len3
+    sum num1, num2
+    print text4,1
+
+    mov rax, 60
+    mov rdi,0
+    syscall
