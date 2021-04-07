bc0a81e93e6040e0d4e511e07499033e89ab638f
ZinaJS <79909126+ZinaJS@users.noreply.github.com>
2021-04-07 14:32:50 +0400

    Update Vector.h

--- Zina1/Vector/Vector.h
+++ Zina1/Vector/Vector.h
@@ -1,20 +1,21 @@
 class Vector {
-	private:
-		int* vector;
-		int size;
-	
-	public:
-		Vector();
-		Vector(int s);
-		Vector(int s, int val);
-		Vector(const Vector &v);
-		
-		int Size();
-		int capacity();
-		void popBack();
-		void pushBack(int val);
-		void insert(int index, int val);
-		void print();
-		bool empty();	
-		int &operator[](int i);
+    private:
+        int* vector;
+        int size;
+
+    public:
+        Vector();
+        Vector(int s);
+        Vector(int s, int val);
+        Vector(const Vector &v);
+
+        int Size();
+        int capacity();
+        void popBack();
+        void pushBack(const int& val);
+        void insert(int index, int val);
+        void print();
+        bool empty();
+        int &operator[](int i);
 };
+
