5a62d6c55953a8fa4486de30638ca2ef114f530e
ZinaJS <79909126+ZinaJS@users.noreply.github.com>
2021-04-07 14:32:15 +0400

    Update Vector.cpp

--- Zina1/Vector/Vector.cpp
+++ Zina1/Vector/Vector.cpp
@@ -1,105 +1,92 @@
 #include <iostream>
 #include <memory.h>
-#include "Vector.h"
+#include "vector.h"
 
 Vector :: Vector() {
-	vector = nullptr;
-	size = 0;
+    vector = nullptr;
+    size = 0;
 }
 
 Vector :: Vector(int s) {
-	size = s;
+    size = s;
 }
 
 Vector :: Vector(int s, int val) {
-	vector = new int[s];
-	
-	for(int i = 1; i <= s; ++i) {
-		vector[i] = val;
-	}
+    vector = new int[s];
+    size = s;
+    for(int i = 0; i < s; ++i) {
+        vector[i] = val;
+    }
 }
 
 Vector :: Vector(const Vector &v) {
-	this -> size = v.size;
-	this -> vector = new int[size];
-	memcpy(this -> vector, v.vector, size * 4);
+    this -> size = v.size;
+    this -> vector = new int[size];
+    memcpy(this -> vector, v.vector, size * 4);
 }
 
 int Vector :: Size() {
-	int s;
-	
-	if(vector != nullptr) {
-		s = sizeof(this -> vector) / sizeof(this -> vector[0]);
-		return s;
-	} else {
-		return 0;
-	}
+    return size;
 }
 
 int Vector :: capacity() {
-	return this -> size + 10;
+    return this -> size + 10;
 }
 
 void Vector :: popBack() {
-	int *newvector = new int[size - 1];
-	
-	for(int i = 0; i < size; ++i) {
-		memcpy(newvector, vector, size * 4);
-	}
-	
-	--size;
-	delete [] vector;
-	vector = newvector;
+    int *newvector = new int[size - 1];
+    memcpy(newvector, vector, (size - 1) * 4);
+    --size;
+    delete [] vector;
+    vector = newvector;
 }
 
-void Vector :: pushBack(int val) {
-	int *newvector = new int[size + 1];
-	
-	for(int i = 0; i <= size; ++i) {
-		memcpy(newvector, vector, size * 4);
-	}
-	
-	++size;
-	delete [] vector;
-	vector = newvector;
-	vector[size - 1] = val;
+void Vector :: pushBack(const int& val) {
+    int *newvector = new int[size + 1];
+    memcpy(newvector, vector, size * 4);
+    newvector[size - 2] = val;
+    delete [] vector;
+    vector = newvector;
+    ++size;
 }
 
 void Vector :: insert(int index, int val) {
-	int *newvector = new int[size + 1];
-	
-	for(int i = 0; i < index; ++i) {
-		newvector[i] = vector[i];
-	}
-	
-	newvector[index] = val;
-	
-	for(int i = index + 1; i <= size; ++i) {
-		newvector[i] = vector[i - 1];
-	}
-	
-	++size;
-	delete [] vector;
-	vector = newvector;
+    int *newvector = new int[size + 1];
+
+    for(int i = 0; i < index; ++i) {
+        newvector[i] = vector[i];
+    }
+
+    newvector[index] = val;
+
+    for(int i = index; i < size; ++i) {
+        newvector[i + 1] = vector[i];
+    }
+
+    ++size;
+    delete [] vector;
+    vector = newvector;
 }
 
 void Vector :: print() {
-	for (int i = 0; i < size; ++i) {
-		std :: cout << vector[i] << ' ';
-	}
+    for (int i = 0; i < size; ++i) {
+        std :: cout << vector[i] << ' ';
+    }
+    std :: cout << std :: endl;
 }
 
 bool Vector :: empty() {
-	if (size == 0) {
-		return true;
-	} else {
-		return false;
-	}
+    if (size == 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 int& Vector :: operator[](int i) {
-    if(i > size) {
-        return -1;
-    } 
-    return arr[i];
+    if (i > size - 1) {
+        std :: cout << "out of range";
+        return vector[0];
+    }
+    return vector[i];
 }
