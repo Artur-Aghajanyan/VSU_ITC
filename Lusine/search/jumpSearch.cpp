d371e7425ad32f6d0325495b6a7f33ce59234296
LusineShahbazyan <79909051+LusineShahbazyan@users.noreply.github.com>
2021-04-15 03:10:49 +0400

    Update jumpSearch.cpp

--- Lusine/search/jumpSearch.cpp
+++ Lusine/search/jumpSearch.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <math.h>
 
-int linerSearch(int *arr, int begin, int end, int number)
+int linearSearch(int *arr, int begin, int end, int number)
 {
     for(int i = begin; i <= end; ++i)
     {
@@ -21,14 +21,14 @@ int jumpSearch(int *arr, int size, int number)
     {
         i += jump;
     }
-        linerSearch(arr, i - jump, std::min(i, size), number);
+        linearSearch(arr, i - jump, std::min(i, size), number);
 }
  
 int main(void)
 {
-    int array[10] = {1,2,3,4,5,6,6,8,9,10};
+    int array[20] = {1, 3, 5, 6, 7, 10, 12, 15, 18, 20, 23, 25, 27, 29, 30, 32, 35, 38, 39, 40};
     int number = 9;
-    int arraySize = 10;
+    int arraySize = 20;
     int result = jumpSearch(array, arraySize - 1, number);
    
     if (result == -1)
