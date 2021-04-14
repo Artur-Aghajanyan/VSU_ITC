a16a0493249d4d19d46696ecdab2d219eda56edb
LusineShahbazyan <79909051+LusineShahbazyan@users.noreply.github.com>
2021-04-15 03:13:08 +0400

    Update binarySearch.cpp

--- Lusine/search/binarySearch.cpp
+++ Lusine/search/binarySearch.cpp
@@ -21,8 +21,7 @@ int binarySearch(int *arr, int begin, int end, int num)
  
 int main(void)
 {
-    int array[20] = {1, 3, 5, 6, 7, 23, 34, 60, 78, 100, 101, 103, 105, 106, 107, 123, 134, 160, 178, 200};
-
+    int array[20] = {1, 3, 5, 6, 7, 10, 12, 15, 18, 20, 23, 25, 27, 29, 30, 32, 35, 38, 39, 40};
     int number = 103;
     int size = 20;
     int result = binarySearch(array, 0, size - 1, number);
