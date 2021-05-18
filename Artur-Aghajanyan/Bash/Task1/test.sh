58f2290f6a721d44b2494ff7958fcf5d260fcee5
Artur Aghajanyan <artur.aghajanyan1997@mail.ru>
2021-05-18 22:51:37 +0400

    Made bash scripts

---
+++ Artur-Aghajanyan/Bash/Task1/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+filenames=`ls`
+for eachfile in $filenames
+do
+	DIRECTORY=${eachfile:0:4}
+
+	if [ ! -d "$DIRECTORY" ]; then
+		mkdir ${eachfile:0:4}	
+	fi
+	
+	mv $eachfile ./$DIRECTORY
+done
