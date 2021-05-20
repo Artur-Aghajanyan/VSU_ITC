4ed7526ffcf59902322e50a146df37144997786b
ZinaJS <79909126+ZinaJS@users.noreply.github.com>
2021-05-20 20:59:24 +0400

    Update game.sh

--- Zina1/bash/19.05/Task 2/game.sh
+++ Zina1/bash/19.05/Task 2/game.sh
@@ -2,21 +2,26 @@
   
 rand=$(($RANDOM %20+1))
 
-for x in {1 .. 3}
+for x in 1 2 3
 do
     echo "Guess the number from 1 to 20: "
     read number
 
-    if (($number == rand))
+    if (($number > 20))
+    then
+        echo "Number should be in 1-20 range. Try again."
+        read number
+    fi
+
+    if (($number == $rand))
     then
         echo "Congretulations! You won the game!"
         break 1
 
-    elif (($number > rand))
+    elif (($number > $rand))
     then
         echo "It should be less than the inputting number."
 
     else
         echo "It should be greater than the inputting number."
     fi
-done
