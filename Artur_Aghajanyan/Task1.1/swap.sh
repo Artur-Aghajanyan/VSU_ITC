99ea3edbc2811b863ca14b6f373f41e7be97bfa9
Artur Aghajanyan <artur.aghajanyan1997@mail.ru>
2021-05-21 10:37:51 +0400

    update File name

---
+++ Artur_Aghajanyan/Task1.1/swap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+GREEN="\e[32m"
+END="\e[0m"
+file1="file1.txt"
+file2="file2.txt"
+echo "Befor swap file1 content: "
+echo $(cat $file1)
+echo "Befor swap file2 content: "
+echo $(cat $file2)
+
+cp $file1 cop_$file1
+cp $file2 $file1
+cp cop_$file1 $file2
+
+echo ""
+echo -e "${GREEN}After swap file1 content: ${END}"
+echo $(cat $file1)
+echo -e "${GREEN}After swap file2 content: ${END}"
+echo $(cat $file2)
