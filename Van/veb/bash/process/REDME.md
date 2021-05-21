d6771569b5244ae8908d026d93affd02726e7b22
Van Marukyan <van.marukyan.1997@gmail.com>
2021-05-21 11:21:11 +0200

    get child process name

---
+++ Van/veb/bash/process/REDME.md
@@ -0,0 +1,2 @@
+for performance of programm must to give any process name, and the programm will return name of all child processis for the given process.
+
---
+++ Van/veb/bash/process/procname.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ARG=$1
+
+function getcname {
+  if [[ -z $ARG ]]; then
+    ARG='init'
+  fi
+  pi=` pidof $ARG `
+  cpids=`pgrep -P $pi`
+  echo $cpids
+
+ps --ppid $pi | awk '{print $4}'
+
+}
+
+getcname $ARG
