f51b6e022bd9283371f1b5a4200466277796b6c7
Van Marukyan <van.marukyan.1997@gmail.com>
2021-05-21 11:45:32 +0200

    auto-format code

--- Van/veb/bash/process/procname.sh
+++ Van/veb/bash/process/procname.sh
@@ -3,13 +3,13 @@ ARG=$1
 
 function getcname {
   if [[ -z $ARG ]]; then
-    ARG='init'
+	ARG='init'
   fi
   pi=` pidof $ARG `
   cpids=`pgrep -P $pi`
   echo $cpids
 
-ps --ppid $pi | awk '{print $4}'
+  ps --ppid $pi | awk '{print $4}'
 
 }
 
