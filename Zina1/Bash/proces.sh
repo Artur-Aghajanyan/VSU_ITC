08ed3917b8b8bd72fe4cd5f2e0b99f92667db8c4
ZinaJS <79909126+ZinaJS@users.noreply.github.com>
2021-05-24 12:11:19 +0400

    Update proces.sh

--- Zina1/Bash/proces.sh
+++ Zina1/Bash/proces.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 function getcpid {
-  tp=`ps x -o %c $1`        #get childs pids of parent pid
-  for i in $tp; do          #loop through childs
-    if [ -z $i ]; then      #check if empty list
-      exit                  #if empty: exit
-    else                    #else
-      echo -n "$i "         #print childs pid
+  tp=`ps x -o "%c" $1`
+  for i in $tp; do
+    if [ -z $i ]; then
+      exit
+    else
+      echo -n "$i "         
     fi;
   done
 }
