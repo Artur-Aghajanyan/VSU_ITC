3e50ac5a77329014afba8c9ec4031fc10f1bf885
ZinaYeghiazaryan <79909126+ZinaYeghiazaryan@users.noreply.github.com>
2021-05-26 01:03:57 +0400

    Update Perfect_Number.py
    Added comments in Perfect_Number.py file.

--- Zina_Yeghiazaryan/python/25_05_2021/Perfect_Number.py
+++ Zina_Yeghiazaryan/python/25_05_2021/Perfect_Number.py
@@ -1,4 +1,8 @@
 def DivSum(n):
+    '''
+        DivSum function has one argument, which should be a natural number.
+        The function counts the sum of divisors of argument.
+    '''
     sum = 0
     for i in range(1, n // 2 + 1):
         if (n % i) == 0:
@@ -6,6 +10,10 @@ def DivSum(n):
     return sum
 
 def Check(n):
+    '''
+        Check function has one argument, which should be a natural number.
+        The function checks if the argumen is perfect.
+    '''
     if (DivSum(n) - n) == 0:
         print(str(n) + " is a perfect number.")
     else:
