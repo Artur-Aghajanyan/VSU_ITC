b073822e7e526c0e96ec129cfe12c4816b97a60f
ZinaYeghiazaryan <79909126+ZinaYeghiazaryan@users.noreply.github.com>
2021-05-25 20:50:12 +0400

    Create thread1.cpp
    Added thread1.cpp file.

---
+++ Zina_Yeghiazaryan/before_bash/c++/thread/thread1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <pthread.h>
+#include <string>
+#include <time.h>
+
+int count = 0;
+pthread_mutex_t mutex;
+
+void* func(void* ch) {
+    pthread_mutex_lock(&mutex);
+    std :: string path = "data.txt";
+    std :: ifstream fin;
+    fin.open(path);
+    if(!fin.is_open()) {
+    	std :: cout << "Open file error!" << std :: endl;
+	}
+	int count = 0;
+	while(!fin.eof()) {
+		fin.get(*(char*)ch);
+		if (*(char*)ch == 'P') {
+			++count;
+			std :: cout << *(char*)ch << count << std :: endl;
+		}
+	}
+	std :: cout << count << std :: endl;
+	fin.close();
+	pthread_mutex_unlock(&mutex);
+	pthread_exit(NULL);
+}
+
+int main() {
+	clock_t Start = clock();
+    char a, b;
+    pthread_t t1, t2;
+    pthread_attr_t attr;
+    int p1 = pthread_attr_init(&attr);
+    int state = PTHREAD_CREATE_JOINABLE;
+    int l1 = pthread_attr_setdetachstate(&attr,state);
+    pthread_mutex_init(&mutex, NULL);
+    pthread_create(&t1, NULL, func, (void*)&a);
+    pthread_create(&t2, NULL, func, (void*)&b);
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+    pthread_mutex_destroy(&mutex);
+    printf ("Time: %.2fs\n", (double)(clock() - Start) / CLOCKS_PER_SEC);
+    pthread_exit(NULL);
+    return 0;
+}
