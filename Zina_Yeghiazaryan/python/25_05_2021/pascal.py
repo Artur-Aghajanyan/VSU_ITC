1ba6d04a22596303e4979e516ecd3ef430eb507f
ZinaYeghiazaryan <79909126+ZinaYeghiazaryan@users.noreply.github.com>
2021-05-27 19:54:09 +0400

    Update and rename Pascal.py to pascal.py
    Changed files name and corrected the code.

--- Zina_Yeghiazaryan/python/25_05_2021/pascal.py
+++ Zina_Yeghiazaryan/python/25_05_2021/pascal.py
@@ -1,4 +1,4 @@
-def Fact(n):
+def fact(n):
     '''
         Fact function has one argument, which should be a natural number.
         The function counts given number's factorial.
@@ -6,24 +6,30 @@ def Fact(n):
     if n == 0 or n == 1:
         return 1
     if n > 1:
-        return Fact(n - 1) * n
+        return fact(n - 1) * n
+        k = 1
+        for i in range(n):
+            k *= i;
+        return k
 
-def Pascal(n):
+    
+def pascal(n):
     '''
         Pascal function has one argument, which should be a natural number (n).
         The function prints out the first n rows of Pascal's triangle.
     '''
     for i in range(0, n+1):
         for j in range(0, i+1):
-            k = Fact(i)/(Fact(j) * Fact(i-j))
+            k = fact(i)/(fact(j) * fact(i-j))
             print(k, end = ' ')
         print("\n")
 
+        
 while True:
     try:
         n = int(input("Enter the number of row: "))
         print("First " + str(n) + " rows of Pascal's triangle: ")
-        Pascal(n)
+        pascal(n)
     except ValueError:
         print("Entered wrong value. Please enter a number.")
         print()
