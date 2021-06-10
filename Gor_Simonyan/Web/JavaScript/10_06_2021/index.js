7fe8099a51dbae847d589e382c7be581a146a006
Gor <gorsimonyan20030707@mail.ru>
2021-06-11 00:09:38 +0400

    Homework for 10.06.2021

---
+++ Gor_Simonyan/Web/JavaScript/10_06_2021/index.js
@@ -0,0 +1,21 @@
+Promise.all([fetch("https://raw.githubusercontent.com/Artur-Aghajanyan/VSU_ITC/html_css/Artur_Aghajanyan/Web/Html_Css/01_06_2021/investments.json"), fetch("https://raw.githubusercontent.com/Artur-Aghajanyan/VSU_ITC/html_css/Artur_Aghajanyan/Web/Html_Css/01_06_2021/users.json")])
+    .then(response => response.map(data => data.json()))
+    .then(async(data) => {
+        return[await data[0], await data[1]]
+    })
+    .then(arrays => {
+        const investments = arrays[0].investments
+        const users = arrays[1].users
+        
+        const data = []
+        investments.filter(value => value.amount > 600).map(value => value.id).forEach(value => data.push(users.find(val => val.id == value)))
+
+        console.log("amount > 600 \n",data)
+
+        const newData = users.map(value => { return{ id: value.id,
+                                                    name: value.name,
+                                                    amount: investments.find(item => item.id == value.id).amount }})
+
+        console.log("Format:{id,name,amount} \n",newData)
+
+    })
