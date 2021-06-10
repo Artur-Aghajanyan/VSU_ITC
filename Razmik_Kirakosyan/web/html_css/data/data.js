7a85e2512821d685cf70744720cd52109652bef1
Razmik <razmikkirakosyan96@gmail.com>
2021-06-10 21:02:55 +0400

    added changes in data.js file.

--- Razmik_Kirakosyan/web/html_css/data/data.js
+++ Razmik_Kirakosyan/web/html_css/data/data.js
@@ -3,7 +3,7 @@ const users = [
     { id: 1, name: "Artur",  age: 24, height: 170 },
     { id: 2, name: "Artur",  age: 22, height: 178 },
     { id: 3, name: "Razmik", age: 24, height: 172 },
-    { id: 5, name: "Gor",     age: 17, height: 180 },
+    { id: 5, name: "Gor",    age: 17, height: 180 },
     { id: 7, name: "Nare",   age: 21, height: 160 },
     { id: 4, name: "Van",    age: 24, height: 170 },
     { id: 6, name: "Zina",   age: 20, height: 154 },
@@ -12,26 +12,23 @@ const users = [
 ]
 
 const investments = [
-    { id: 1, amount: 340},
-    { id: 2, amount: 356},
-    { id: 3, amount: 657},
-    { id: 4, amount: 2434},
-    { id: 5, amount: 824},
-    { id: 6, amount: 567},
-    { id: 7, amount: 123},
-    { id: 8, amount: 678},
-    { id: 9, amount: 576},
+    { id: 1,  amount: 340},
+    { id: 2,  amount: 356},
+    { id: 3,  amount: 657},
+    { id: 4,  amount: 2434},
+    { id: 5,  amount: 824},
+    { id: 6,  amount: 567},
+    { id: 7,  amount: 123},
+    { id: 8,  amount: 678},
+    { id: 9,  amount: 576},
     { id: 10, amount: 236},
 ]
 
-//const data=[]
-//investments.filter(value => value.amount>600).map(value => value.id).forEach(value data.push (users.find(item => item.id == value)
-//{return item.name}));
-//	console.log(data);
-
+const aaa = investments.filter(value => value.amount > 600)
+console.log(users.filter(user => aaa .find(investment => investment.id === user.id)).map(user => user.name))
 
 const newarr=users.map( item => { return { id: item.id, name: item.name, amount: investments.find(value => value.id == item.id).amount}});
-	console.log("print new array by this format \n", newarr);
+	console.log("print new array by this format  \n", newarr);
 
 
 
