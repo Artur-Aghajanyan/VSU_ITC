a23bf3506a34fe3b2b1b59ac2007044a5cb56e30
LusineShahbazyan <shahbazyanl.e@gmail.com>
2021-06-15 14:11:37 +0400

    Make some changes

--- Lusine_Shahbazyan/web/funcArray/script.js
+++ Lusine_Shahbazyan/web/funcArray/script.js
@@ -1,17 +1,19 @@
-f1 = () => "hello";
+f1 = () => () => () => () => "hello";
 f2 = () => () => "edabit";
 f3 = () => () => () => "user";
 let funcArray = [f2, f3, f1];
 
 console.log("Befor sorting");
 console.log(funcArray);
+
 let count = 0;
 function countOfCall(func)
 {
-    count++;
-    if (typeof(func()) === 'function')
+    if (typeof(func) === 'function')
     {
+        count ++;
         countOfCall(func());
+
     }
     return count;
 }
@@ -29,7 +31,9 @@ function funcSort(arr)
     {
         for (let j = 0; j < arr.length - i - 1; j++)
         {
+            count = 0
             let value1 = countOfCall(arr[j]);
+            count = 0
             let value2 = countOfCall(arr[j + 1]);
             if(value1 > value2)
             {
@@ -43,4 +47,3 @@ function funcSort(arr)
 let sortedArray = funcSort(funcArray);
 console.log("After sorting")
 console.log(sortedArray)
-
