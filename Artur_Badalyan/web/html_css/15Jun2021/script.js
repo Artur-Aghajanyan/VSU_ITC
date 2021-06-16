64ecbe8c18338229c0d5dce20ccf129236bc327b
Artur <badalyannaaa@gmail.com>
2021-06-16 17:39:33 +0400

    Made pagination logic(with class)

--- Artur_Badalyan/web/html_css/15Jun2021/script.js
+++ Artur_Badalyan/web/html_css/15Jun2021/script.js
@@ -1,77 +1,84 @@
-let items = [];
-let pageCount = 150;
-let pageSize = 10;
+class Pagination {
+    constructor(pageCount,pageSize) {
+        this.pageCount = pageCount;
+        this.pageSize = pageSize;
+        this.items = [];
 
-function printPage(array,start,finish){
-    let arr = [];
-    index = 0;
-    for (let i = start;i < finish;i++) {
-        arr[index] = array[i];
-        index++;
+        this.last = this.pageSize;
+        this.start = 0;
+        this.lastItems = this.last;
+
+        for (let i = 0; i < this.pageCount; i++) {
+            this.items[i] = i;
+        }
+
+        this.printPage(this.start,this.last);
     }
-    console.log(arr)
-}
 
-for (let i = 0; i < pageCount; i++) {
-    items[i] = i;
-}
-
-let last = pageSize;
-let start = 0;
-let lastItems = last;
-
-printPage(items,start,last);
-
-function nextPage() {
-    console.log("Next Page");
-    start = lastItems;
-    lastItems = start + last;
-    if (lastItems > pageCount) {
-        throw new Error("There is no such page")
+    printPage(start,finish){
+        let arr = [];
+        let index = 0;
+        for (let i = start;i < finish;i++) {
+            arr[index] = this.items[i];
+            index++;
+        }
+        console.log(arr)
     }
-    printPage(items,start,lastItems);
-    console.log("----------");
-}
 
-function prevPage() {
-    console.log("Previous Page");
-    start = start - last;
-    lastItems = lastItems - last;
-    if (start < 0) {
-        throw new Error("There is no such page")
+    nextPage() {
+        this.start = this.lastItems;
+        this.lastItems = this.start + this.last;
+        if (this.lastItems > this.pageCount) {
+            throw new Error("There is no such page")
+        }
+        this.printPage(this.start,this.lastItems);
     }
-    printPage(items,start,lastItems);
-    console.log("----------");
-}
 
-function goToPage(page) {
-    if (page > pageCount/pageSize || page < 1){
-        throw new Error("There is no such page")
+    prevPage() {
+        this.start = this.start - this.last;
+        this.lastItems = this.lastItems - this.last;
+        if (this.start < 0) {
+            throw new Error("There is no such page")
+        }
+        this.printPage(this.start,this.lastItems);
     }
-    console.log("Go to Page ", page);
-    lastItems = last * page;
-    start = lastItems - last;
-    printPage(items,start,lastItems);
+
+    goToPage(page) {
+        if (page > this.pageCount/this.pageSize || page < 1){
+            throw new Error("There is no such page")
+        }
+        console.log("Go to Page ", page);
+        this.lastItems = this.last * page;
+        this.start = this.lastItems - this.last;
+        this.printPage(this.start,this.lastItems);
+    }
+
+    lastPage() {
+        this.lastItems = this.pageCount;
+        this.start = this.lastItems - this.last;
+        this.printPage(this.start,this.lastItems)
+    }
+
+    firstPage() {
+        this.start = 0;
+        this.lastItems = this.last;
+        this.printPage(this.start,this.lastItems)
+    }
+
 }
 
-function lastPage() {
-    console.log("Last Page");
-    lastItems = pageCount;
-    start = lastItems - last;
-    printPage(items,start,lastItems)
-}
+let pageObj = new Pagination(150,10);
 
-function firstPage() {
-    console.log("First Page");
-    start = 0;
-    lastItems = last;
-    printPage(items,start,lastItems)
-}
-
-nextPage();
-nextPage();
-nextPage();
-prevPage();
-goToPage(15);
-firstPage();
-lastPage();
+console.log("Next Page");
+pageObj.nextPage();
+console.log("Next Page");
+pageObj.nextPage();
+console.log("Next Page");
+pageObj.nextPage();
+console.log("Previous Page");
+pageObj.prevPage();
+pageObj.goToPage(15);
+console.log("First Page");
+pageObj.firstPage();
+console.log("Last Page");
+pageObj.lastPage();
