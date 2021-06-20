4a4350fe364e92aac392f637c63eb466d0043932
Artur <badalyannaaa@gmail.com>
2021-06-21 01:09:44 +0400

    Autentication (update)

--- Artur_Badalyan/web/html_css/16Jun2021/script.js
+++ Artur_Badalyan/web/html_css/16Jun2021/script.js
@@ -1,58 +1,128 @@
-function Role(login, password, role) {
-    this.login = login;
-    this.password = password;
-    this.role = role;
-    putRole(role);
-}
+registrArray = [];
 
-function User() {
-    this.firstName = "";
-    this.lastName = "";
-    this.age = null;
+class Role {
+    firstName = '';
+    lastName = '';
+    age = null;
 
-    this.setPersonData = function (firstName, lastName, age) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-    };
-
-    this.getPersonData = function () {
-        return "First Name: " + this.firstName + "Last Name: " + this.lastName + "Age: " + this.age;
-    };
-}
-
-
-function Admin() {
-    this.firstName = "";
-    this.lastName = "";
-    this.age = null;
-
-    this.setPersonData = function (firstName, lastName, age) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-    };
-
-    this.getPersonData = function () {
-        return "First Name: " + this.firstName + "Last Name: " + this.lastName + "Age: " + this.age;
-    };
-}
-
-function putRole(role) {
-    if (role.toUpperCase() === "ADMIN") {
-        Role.prototype = new Admin();
-        Role.prototype.constructor = Role;
-    } else {
-        Role.prototype = new User();
-        Role.prototype.constructor = Role;
+    readContent() {
+        console.log("This is a content");
     }
 }
 
-let users = [
-    new Role("Karen", "3333", "user"),
-    new Role("Tigran", "14253678", "Admin"),
-    new Role("Arman", "password78", "user")
-];
+class CreateRole {
+    constructor(login, password, role) {
+        registrArray.forEach(item => {
+            if (item.login === login) {
+                throw("This login already created");
+            }
+        });
+        if (role === 'user') {
+            registrArray.push({login: login, password: password, role: role});
+            console.log("Created user and role: ", role);
+        } else {
+            console.log("The role can't be ", role);
+        }
+    }
+}
+
+class User extends Role {
+    constructor() {
+        super();
+        this.currRole = 'guest';
+    }
+
+    login(login, password) {
+        for (let i = 0; i < registrArray.length; i++) {
+            if (registrArray[i].login === login && registrArray[i].password === password && this.currRole === "guest") {
+                this.currRole = "user";
+                console.log("You are successfully logged in, Hi ", this.firstName);
+                break;
+            } else if (this.currRole === "user") {
+                console.log("You are already logged in");
+                break;
+            } else if (registrArray[i].login === login && registrArray[i].password === password && this.currRole === "admin") {
+                console.log("You are successfully logged in, Hi ", this.firstName);
+                break;
+            } else {
+                console.log("Your login or password is incorrect");
+                break;
+            }
+        }
+    }
+
+
+    logOut() {
+        if (this.currRole === "user" || this.currRole === "admin") {
+            this.currRole = "guest";
+            console.log("So early ?, Byee ", this.firstName);
+        } else {
+            console.log("Тo log out of your account, you need to login !!!");
+        }
+    }
+
+    setPersonData(firstName, lastName, age) {
+        if (this.currRole === "user") {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        } else {
+            console.log("To change your personal data, you need to register !!!");
+        }
+
+    }
+
+    getPersonData() {
+        if (this.currRole === "user") {
+            console.log("First name: ", this.firstName, " Last name: ", this.lastName, " Age: ", this.age);
+        } else {
+            console.log("To get your personal data, you need to register !!!");
+        }
+    }
+}
+
+class Admin extends User {
+    constructor() {
+        super();
+        this.currRole = "admin";
+    }
+
+    delUser(login) {
+        for (let i = 0; i < registrArray.length; i++) {
+            if (registrArray[i].login === login) {
+                registrArray = registrArray.splice(i, i);
+                break;
+            }
+        }
+        console.log("User ", login, " deleted");
+    }
+}
+
+console.log("Registration (fail)");
+let guest1 = new CreateRole("Karen", "pass", "usrrrr");
+console.log("Registration");
+let guest2 = new CreateRole("Karen", "pass", "user");
+// Creating user object
+let user = new User();
+console.log("User login (fail)");
+user.login("Karens", "asd");
+console.log("User login");
+user.login("Karen", "pass");
+console.log("User set and get personal data");
+user.setPersonData("Karen", "Harutyunyan", 22);
+user.getPersonData();
+// Creating admin object
+let admin = new Admin();
+console.log("Admin login");
+admin.login("Karen", "pass");
+console.log("Admin logout");
+admin.logOut();
+console.log("Deleting user from admin");
+admin.delUser("Karen");
+
+
+
+
 
 
 
