56153de9d882451cf3d40796a1c3e2632cfbdbe5
Gor07s <79909074+Gor07s@users.noreply.github.com>
2021-06-28 16:55:44 +0400

    Update Work.sql

--- Gor_Simonyan/Web/DataBase/25_06_2021/Work.sql
+++ Gor_Simonyan/Web/DataBase/25_06_2021/Work.sql
@@ -10,13 +10,13 @@ insert into Actives(id,name,surname,s_id) values(1,"Ani","Darbinyan",2),(2,"Hako
 create table Projects(id INT NOT NULL PRIMARY KEY,name VARCHAR(255) NOT NULL);
 insert into Projects(id,name) values(1,"Site"),(2,"art. intelligence"),(3,"Excel"),(4,"Web Server");
 
-create table Proj_Act(p_id iNT NOT NULL,a_id INT NOT NULL,Work_Date_start VARCHAR(255) NOT NULL,Work_Date_end VARCHAR(255),Time INT NOT NULL);
-insert into Proj_Act(p_id,a_id,Work_Date_start,Work_Date_end,Time) values(1,1,"23.06.2021","25.06.2021",7),(1,5,"23.06.2021","24.06.2021",4),(1,3,"23.06.2021","24.06.2021",5),(2,2,"20.06.2021","26.06.2021",23),(2,3,"20.06.2021","NULL",42),(3,4,"24.06.2021","25.06.2021",10),(4,1,"23.06.2021","25.06.2021",6),(4,2,"23.06.2021","26.06.2021",7),(4,5,"23.06.2021","25.06.2021",2);
+create table Proj_Act(p_id iNT NOT NULL,a_id INT NOT NULL,Work_Date_start DATETIME NOT NULL,Work_Date_end DATETIME);
+insert into Proj_Act(p_id,a_id,Work_Date_start,Work_Date_end) values(1,1,"2021-06-23 14:02:00","2021-06-28 16:20:00"),(1,5,"2021-06-22 12:00:00","2021-06-25 12:00:00"),(1,3,"2021-06-22 23:10:00","2021-06-28 16:20:00"),(2,2,"2021-06-19 11:00:00","2021-06-25 20:20:00"),(2,3,"2021-06-23 09:05:00",NULL),(3,4,"2021-06-27 14:05:00","2021-06-28 23:50:00"),(4,1,"2021-06-20 14:00:00","2021-06-24 16:40:00"),(4,2,"2021-06-23 14:00:00","2021-06-24 16:47:00"),(4,5,"2021-06-23 17:02:00","2021-06-28 17:00:00");
 
 select Projects.id as Projects_id,Projects.name as Projects_name,Actives.name as Active_name,Actives.surname as Active_surname from Projects inner join Actives inner join Proj_Act where Projects.id = Proj_Act.p_id and Actives.id = Proj_Act.a_id;
 
-select p_id as Project_id, sum(Time) as Time from Proj_Act GROUP BY p_id;
+select p_id as project_id, sum(time_format(timediff(ifnull(Work_Date_end,now()),Work_Date_start),'%H:%i:%s')) / 3 as Time from Proj_Act GROUP BY p_id;
 
-select id,name as project,Work_Date_start,Time from Projects inner join Proj_Act where Projects.id = Proj_Act.p_id and Proj_Act.Work_Date_end = "NULL";
+select id,name as project,Work_Date_start from Projects inner join Proj_Act where Projects.id = Proj_Act.p_id and Proj_Act.Work_Date_end is NULL;
 
-drop database data;
+drop database data; 
