0faaa67236d20228382fd046cf2b40bdfb0e6120
Artur Aghajanyan <artur.aghajanyan1997@mail.ru>
2021-06-30 14:39:53 +0400

    update

--- Artur_Aghajanyan/BackEnd/29_06_2021/index.js
+++ Artur_Aghajanyan/BackEnd/29_06_2021/index.js
@@ -2,14 +2,22 @@ const findInFiles = require('find-in-files');
 const EventEmitter = require('events');
 
 class MyEmitter extends EventEmitter {}
-const myEmitter = new MyEmitter();
-myEmitter.on('event', () => console.log('an event occurred'));
 
-findInFiles.find('Lorem', './', 'text.txt').then(function(results) {
-  for (var result in results) {
-    var res = results[result];
+const myEmitter = new MyEmitter();
+myEmitter.on('event', () => {
+  // eslint-disable-next-line no-console
+  console.log('an event occurred');
+  return 0;
+});
+
+findInFiles.find('Lorem', './', 'text.txt').then((results) => {
+  // eslint-disable-next-line guard-for-in,no-restricted-syntax
+  for (const result in results) {
+    const res = results[result];
+    // eslint-disable-next-line no-console
+    console.log(res);
     myEmitter.emit('event');
     return 0;
   }
+  return 1;
 });
-
