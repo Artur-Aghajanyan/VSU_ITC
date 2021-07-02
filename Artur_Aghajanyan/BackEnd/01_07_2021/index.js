4d4e5a568bc13d144cee7d8988885fa9ee240774
Artur Aghajanyan <artur.aghajanyan1997@mail.ru>
2021-07-02 19:03:49 +0400

    update

--- Artur_Aghajanyan/BackEnd/01_07_2021/index.js
+++ Artur_Aghajanyan/BackEnd/01_07_2021/index.js
@@ -1,33 +1,109 @@
 const express = require('express');
 const fs = require('fs');
-
+const bodyParser = require('body-parser');
 const app = express();
 
 const host = '127.0.0.1';
 const port = 7000;
 
+app.use(bodyParser.json());
 
+// Middleware Start
 let checkFile = function(req, res, next) {
   fs.readFile('text.txt', 'utf8', (err, data) => {
     if (err) {
       throw err;
     }
-    const arr = data.toString().replace(/\r\n/g,'\n').split('\n');
+    const arr = data.toString().replace(/\r\n/g, '\n').split('\n');
 
-    if(arr.length < req.query.id) {
+    if (arr.length < req.query.id) {
       res.status(404).send(`In File Not found Line ${req.query.id}`);
     }
+    else {
+      next();
+    }
   });
-}
+};
 
-app.get('/home',checkFile, (req, res) => {
-  console.log("aaaa");
+let checkNumber = (req, res, next) => {
+  const checkNum = /^[A-Za-z]+$/;
+  const text = req.body.text;
+  if (checkNum.test(text)) {
+    next();
+  }
+  else {
+    res.end('Error');
+  }
+};
+// Middleware End
+
+app.get('/get', checkFile, (req, res) => {
   fs.readFile('text.txt', 'utf8', (err, data) => {
     if (err) {
       throw err;
     }
-    const arr = data.toString().replace(/\r\n/g,'\n').split('\n');
+    const arr = data.toString().replace(/\r\n/g, '\n').split('\n');
     res.send(arr[req.query.id - 1]);
+  });
+});
+
+app.post('/post', checkNumber, (req, res) => {
+  const text = req.body.text;
+  fs.appendFile('text.txt', `\n${text}`, (err, data) => {
+    if (err) {
+      throw err;
+    }
+    res.status(200);
+    res.end('Append Text into file');
+  });
+});
+
+app.put('/put', checkNumber, checkFile, (req, res) => {
+  const text = req.body.text;
+  fs.readFile('text.txt', 'utf8', (err, data) => {
+    if (err) {
+      throw err;
+    }
+    const arr = data.toString().replace(/\r\n/g, '\n').split('\n');
+    res.send(arr[req.query.id - 1]);
+    let id = req.query.id;
+    arr[id - 1] = text;
+    let newArr = [];
+    arr.forEach((item, index) => {
+      console.log(item, index);
+      arr[index] = item + '\n';
+    });
+    fs.writeFile('text.txt', arr.join(''), (err, data) => {
+      if (err) {
+        throw err;
+      }
+      res.status(200);
+      res.end(`Change Line ${id} to ${text}`);
+    });
+  });
+});
+
+app.delete('/delete', checkFile, (req, res) => {
+  fs.readFile('text.txt', 'utf8', (err, data) => {
+    if (err) {
+      throw err;
+    }
+    const arr = data.toString().replace(/\r\n/g, '\n').split('\n');
+    res.send(arr[req.query.id - 1]);
+
+    let id = req.query.id;
+    arr.splice(id - 1, 1);
+    arr.forEach((item, index) => {
+      console.log(item, index);
+      arr[index] = item + '\n';
+    });
+    fs.writeFile('text.txt', arr.join(''), (err, data) => {
+      if (err) {
+        throw err;
+      }
+      res.status(200);
+      res.end(`Delete Text from Line ${id} Into File`);
+    });
   });
 });
 
@@ -37,3 +113,4 @@ app.use((req, res, next) => {
 });
 
 app.listen(port, host, () => console.log(`Server listens http://${host}:${port}`));
+
