8c000ea23452ff2ff23c418f4346cf844e67b4a1
anna2399 <annamayilyan99@gmail.com>
2021-07-15 11:58:48 +0400

    Added homework for 14Jul2021

---
+++ Anna_Mayilyan/web/Swagger/index.js
@@ -0,0 +1,69 @@
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+var express = require('express');
+var app = express();
+
+app.get('/senior', (req, res) => {
+    res.send({
+		id: 2,
+        lastName: "Walker",
+        firstName: "John"
+    });
+
+});
+
+app.delete('/senior', (req, res) => {
+    res.send({
+        id: 3,
+		lastName: "Myers",
+		firstName: "Mike"
+    });
+});
+app.put('/middle', (req, res) => {
+    res.send({
+        id: 4,
+		lastName: "Brown",
+		firstName: "Tom"
+    });
+});
+app.delete('/middle', (req, res) => {
+    res.send({
+        id: 5,
+		lastName: "Styles",
+		firstName: "Tomas"
+    });
+});
+app.get('/junior', (req, res) => {
+    res.send({
+		id: 4,
+		lastName: "Herron",
+		firstName: "Mary",
+    });
+});
+app.post('/junior', (req, res) => {
+    res.send({
+		id: 5,
+        lastName: "Brown",
+		firstName: "Jonas",
+    });
+});
+app.post('/intern', (req, res) => {
+    res.send({
+		id: 7,
+        firstName: "Kate",
+		lastName: "Rolling"
+    });
+});
+app.put('/intern', (req, res) => {
+    res.send({
+		id: 3,
+        firstName: "Mike",
+		lastName: "Brown"
+    });
+});
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.listen(3000);
+
+
+
