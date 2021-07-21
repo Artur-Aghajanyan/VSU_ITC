6e3dbb069904df09382449656eff8e08efd964e2
Artur-Aghajanyan <53654368+Artur-Aghajanyan@users.noreply.github.com>
2021-07-21 10:39:53 +0400

    Update script.py

--- Artur_Aghajanyan/QA/21_07_2021/script.py
+++ Artur_Aghajanyan/QA/21_07_2021/script.py
@@ -2,7 +2,7 @@ import os
 
 comand = "jmeter -n -t ./test.jmx -JcountOfThread=20 -f -l ./result.jtl"
 result = "./result.jtl"
-agent = "./startAgent.sh"
+agent = "startAgent.sh"
 generator = "JMeterPluginsCMD.sh --generate-png ./image.png --input-jtl ./result.jtl --plugin-type ResponseTimesOverTime --width 800 --height 600"
 
 if os.path.exists(result):
