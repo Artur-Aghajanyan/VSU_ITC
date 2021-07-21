db1ac6e63f261ef9e62f7e2c484b3aa20c76b295
Artur Aghajanyan <artur.aghajanyan1997@mail.ru>
2021-07-21 10:05:57 +0400

    made jmeter cmd

---
+++ Artur_Aghajanyan/QA/21_07_2021/script.py
@@ -0,0 +1,16 @@
+import os
+
+comand = "jmeter -n -t ./test.jmx -JcountOfThread=20 -f -l ./result.jtl"
+result = "./result.jtl"
+agent = "./startAgent.sh"
+generator = "JMeterPluginsCMD.sh --generate-png ./image.png --input-jtl ./result.jtl --plugin-type ResponseTimesOverTime --width 800 --height 600"
+
+if os.path.exists(result):
+    os.remove(result)
+    
+if os.path.exists(image):
+    os.remove(image)
+
+os.system(agent)
+os.system(comand)
+os.system(generator)
